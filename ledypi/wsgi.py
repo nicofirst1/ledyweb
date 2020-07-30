5020f35ddcb43c6decfa55765dd246a4847d7a8f
dizzi <nicofirst1@gmail.com>
2020-07-30 11:41:09 +0200

    renamed project

--- ledypi/wsgi.py
+++ ledypi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for LEDyPi project.
+WSGI config for ledypi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LEDyPi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ledypi.settings')
 
 application = get_wsgi_application()
